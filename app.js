5eec34c6e15e1fc8e2ca5c908a1d33c522a6263d
Peter Mulindwa <petermulindwa30@gmail.com>
2023-04-07 12:58:23 +0300

    Updated home content and styling

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const sections = document.querySelectorAll('.section');
 const sectBtns = document.querySelectorAll('.controls');
 const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('main');
+const allSections = document.querySelector('.main-content');
 
 function PageTransitions(){
     for(let i=0; i<sectBtn.length; i++){
@@ -11,6 +11,25 @@ function PageTransitions(){
             this.classList += 'active-btn';
         });
     }
+// section active
+    allSections.addEventListener('click', (e)=>{
+        const id = e.target.dataset.id;
+        if(id){
+            // remove selected from other buttons
+            sectBtns.forEach((btn)=>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active');
+            // hide other sections
+
+            sections.forEach((section)=>{
+                section.classList.remove('active');
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
+    })
 }
 
 
